ad3c51dc6f5c3784b37d83943918ac2ca952b5c7
omkar <omkar3524@gmail.com>
2019-10-29 11:38:53 +0530

    array

---
+++ array.php
@@ -0,0 +1,71 @@
+<?php
+
+//packages
+$package_Key = ['length_measutements' => 'length_measurements'];
+
+//users
+$user_key = ['user_firstname' => 'firstname',
+            'user_lastname' => 'lastname',
+            'user_email' => 'email',
+            'user_password' => 'password',
+            'user_registered' =>   'registered',
+            'user_avatar' => 'avatar',
+            'user_salt' =>    'salt'    
+];
+
+//nodes
+$node_key= [
+    'node_alias' => 'alias',
+    'node_title' => 'title',
+    'node_content' => 'content',
+    'node_parent' => 'parent',
+    'node_order' => 'order',
+    'node_type' => 'type',
+    'node_status' => 'status'
+];
+
+//profiles
+$profile_key = [
+    'billing_firstname' => 'firstname',
+    'billing_lastname' => 'lastname',
+    'billing_cardtype' => 'cardtype',
+    'billing_cardnumber' => 'cardnumber',
+    'billing_cardmonth' => 'cardmonth',
+    'billing_cardyear' => 'cardyear',
+    'billing_code' => 'code',
+    'billing_email' => 'email',
+    'billing_phone1' => 'phone1',
+    'billing_phone2' => 'phone2',
+    'billing_company' => 'company',
+    'billing_street' => 'street',
+    'billing_district' => 'district',
+    'billing_city' => 'city',
+    'billing_state' => 'state',
+    'billing_zipcode' => 'zipcode',
+
+];
+//resources
+$resource_key = [
+    'resource_name' => 'name',
+    'resource_description' => 'description',
+    'resource_path' => 'path',
+    'resource_type' => 'type',
+];
+
+//terms
+$term_key = [
+    'term_name' => 'name',
+    'term_description' => 'description',
+];
+//comments
+$comment_key = [
+    'comment_username' => 'username',
+    'comment_useremail' => 'useremail',
+    'comment_userwebsite' => 'userwebsite',
+    'comment_ip' => 'ip',
+    'comment_parent' => 'parent',
+    'comment_title' => 'title',
+    'comment_content' => 'content',
+    'comment_type' => 'type',
+    'comment_status' => 'status',
+];
